bec0239dc492495ea18f0cc184b3355c67023778
Arnold Krille <a.krille@b-c-s.de>
2014-01-28 15:38:29 +0100

    increase version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       "Jesse Nelson"
 maintainer_email "spheromak@gmail.com"
 description      "Redhat pxeos/pxeboot/netboot management"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "2.0.1"
+version          "2.0.2"
 
 %w{ centos rhel ubuntu }.each do |os|
   supports os
